ed40606ba07ee72d6469f51e7d5f8d2ea4c72d38
Govind Kamtamneni <govind.k@microsoft.com>
2022-08-22 20:06:27 -0700

    publish orders to kafka topic

--- order-service/src/main/java/com/microsoft/gbb/rasa/orderservice/OrderServiceApplication.java
+++ order-service/src/main/java/com/microsoft/gbb/rasa/orderservice/OrderServiceApplication.java
@@ -2,7 +2,9 @@ package com.microsoft.gbb.rasa.orderservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.kafka.annotation.EnableKafka;
 
+@EnableKafka
 @SpringBootApplication
 public class OrderServiceApplication {
 
