6f7751c860eef5f4b2dcb368bead08d5d4b6366c
Govind Kamtamneni <govind.k@microsoft.com>
2022-09-09 17:12:05 -0700

    initialize virtual customer svc; closes #21

---
+++ virtual-worker/README.md
@@ -0,0 +1,14 @@
+# virtual-worker
+Based on [Virtual Worker](https://github.com/Azure/reddog-code/tree/master/RedDog.VirtualWorker) from [reddog-code](https://github.com/Azure/reddog-code) repurposed for Java.
+## How to run
+### Prerequisites
+- Java 11
+- Maven 3
+- Docker
+- Docker Compose
+- Dapr
+## Run
+- `mvn clean package`
+- `docker-compose up`
+
+## Test
---
+++ virtual-worker/src/main/java/com/microsoft/gbb/reddog/virtualworker/VirtualWorkerApplication.java
@@ -0,0 +1,13 @@
+package com.microsoft.gbb.reddog.virtualworker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VirtualWorkerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VirtualWorkerApplication.class, args);
+    }
+
+}
