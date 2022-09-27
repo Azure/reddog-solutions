7dcb33604abd5f084d4a34ecbee37c060b549e39
Govind Kamtamneni <govind.k@microsoft.com>
2022-09-26 17:24:36 -0700

    add feign client ex to showcase service calls native to spring cloud

--- loyalty-service/src/main/java/com/microsoft/gbb/reddog/loyaltyservice/LoyaltyServiceApplication.java
+++ loyalty-service/src/main/java/com/microsoft/gbb/reddog/loyaltyservice/LoyaltyServiceApplication.java
@@ -2,8 +2,10 @@ package com.microsoft.gbb.reddog.loyaltyservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class LoyaltyServiceApplication {
 
     public static void main(String[] args) {
