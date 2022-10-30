805d12826a122aa24003a8287feb4e7ab0d607f1
Govind Kamtamneni <govind.k@microsoft.com>
2022-10-29 22:21:07 -0700

    add nextjs react version of reddog ui

---
+++ ui/nextjs-react/src/navigation/vertical/index.ts
@@ -0,0 +1,31 @@
+// ** Icon imports
+import HomeOutline from 'mdi-material-ui/HomeOutline'
+import EmailOutline from 'mdi-material-ui/EmailOutline'
+import ShieldOutline from 'mdi-material-ui/ShieldOutline'
+
+// ** Type import
+import { VerticalNavItemsType } from 'src/@core/layouts/types'
+
+const navigation = (): VerticalNavItemsType => {
+  return [
+    {
+      title: 'Home',
+      icon: HomeOutline,
+      path: '/home'
+    },
+    {
+      title: 'Second Page',
+      icon: EmailOutline,
+      path: '/second-page'
+    },
+    {
+      title: 'Access Control',
+      icon: ShieldOutline,
+      path: '/acl',
+      action: 'read',
+      subject: 'acl-page'
+    }
+  ]
+}
+
+export default navigation
