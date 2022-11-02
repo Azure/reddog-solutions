2cf8ee28fa0f28bafa7f784746a8d57ba94dbb58
Govind Kamtamneni <govind.k@microsoft.com>
2022-11-02 08:43:33 -0700

    move frontend to its own repo and refer to it via submodule

--- order-service/src/main/java/com/microsoft/gbb/rasa/orderservice/OrderServiceApplication.java
+++ order-service/src/main/java/com/microsoft/gbb/rasa/orderservice/OrderServiceApplication.java
@@ -1,6 +1,5 @@
 package com.microsoft.gbb.rasa.orderservice;
 
-import com.microsoft.applicationinsights.attach.ApplicationInsights;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
@@ -12,7 +11,6 @@ import org.springframework.kafka.annotation.EnableKafka;
 public class OrderServiceApplication {
 
     public static void main(String[] args) {
-        // ApplicationInsights.attach();
         SpringApplication.run(OrderServiceApplication.class, args);
     }
 
