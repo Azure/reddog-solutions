5f5a8bec562d8cdb3e2df02587ab7ad87e7f8db8
Govind Kamtamneni <govind.k@microsoft.com>
2022-11-04 07:00:49 -0700

    add translate and update endpoints

--- nn-bindings/project/app/main.py
+++ nn-bindings/project/app/main.py
@@ -9,7 +9,7 @@ async def pong():
     return {"status": "Ok"}
 
 
-@app.get("/generate-product-names", response_model=str)
+@app.get("/generate/product-names", response_model=str)
 async def generate_product_names(prompt: str):
     response = openai.Completion.create(
         engine="text-davinci-002",
@@ -20,5 +20,19 @@ async def generate_product_names(prompt: str):
         frequency_penalty=0,
         presence_penalty=0,
         stop=None
+    )
+    return response.choices[0].text
+
+@app.get("/translate/to-french", response_model=str)
+async def generate_product_names(prompt: str):
+    response = openai.Completion.create(
+        engine="generate_description",
+        prompt=prompt,
+        temperature=0.5,
+        max_tokens=100,
+        top_p=1,
+        frequency_penalty=0,
+        presence_penalty=0,
+        stop=["\n"]
     )
     return response.choices[0].text
