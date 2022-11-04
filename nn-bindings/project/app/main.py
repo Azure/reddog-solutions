f6f22a8839a4ea50edc22613fb1874058e78497f
Govind Kamtamneni <govind.k@microsoft.com>
2022-11-04 06:15:30 -0700

    add openai endpoint

---
+++ nn-bindings/project/app/main.py
@@ -0,0 +1,24 @@
+from fastapi import Depends, FastAPI
+from .config import settings
+import openai
+
+app = FastAPI()
+
+@app.get("/health")
+async def pong():
+    return {"status": "Ok"}
+
+
+@app.get("/generate-product-names", response_model=str)
+async def generate_product_names(prompt: str):
+    response = openai.Completion.create(
+        engine="text-davinci-002",
+        prompt=prompt,
+        temperature=0.8,
+        max_tokens=60,
+        top_p=1,
+        frequency_penalty=0,
+        presence_penalty=0,
+        stop=None
+    )
+    return response.choices[0].text
