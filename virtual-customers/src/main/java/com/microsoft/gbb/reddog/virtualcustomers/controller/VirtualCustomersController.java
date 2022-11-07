36be18d42f06169fdf80e3aed7d71bf40a8fba38
Govind Kamtamneni <govind.k@microsoft.com>
2022-11-06 21:23:56 -0800

    fix order creation endpoint from frontend

--- virtual-customers/src/main/java/com/microsoft/gbb/reddog/virtualcustomers/controller/VirtualCustomersController.java
+++ virtual-customers/src/main/java/com/microsoft/gbb/reddog/virtualcustomers/controller/VirtualCustomersController.java
@@ -1,12 +1,17 @@
 package com.microsoft.gbb.reddog.virtualcustomers.controller;
 
 
+import com.microsoft.gbb.reddog.virtualcustomers.model.CustomerOrder;
 import com.microsoft.gbb.reddog.virtualcustomers.service.OrderCreationJobService;
+import lombok.extern.slf4j.Slf4j;
 import org.jobrunr.scheduling.JobScheduler;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
+@Slf4j
 public class VirtualCustomersController {
 
     private final JobScheduler jobScheduler;
@@ -17,10 +22,17 @@ public class VirtualCustomersController {
         this.orderCreationJobService = orderCreationJobService1;
     }
 
-    @GetMapping(value = "/simulate-orders")
+    @PostMapping(value = "/stimulate-orders")
     @CrossOrigin(origins = "*")
-    public ResponseEntity<String> createOrderJob(@RequestParam(value = "numOrders", defaultValue = "10") int numOrders) {
+    public ResponseEntity<List<CustomerOrder>> createOrderJob(
+            @RequestParam(value = "numOrders", defaultValue = "1") int numOrders,
+            @RequestHeader(value="x-source", required = false) String origin) {
+        if (numOrders < 1) {
+            return ResponseEntity.badRequest().build();
+        }
+        log.info("Creating {} orders from {}", numOrders, origin);
+        List<CustomerOrder> orders = orderCreationJobService.createOrders(numOrders);
         jobScheduler.enqueue(orderCreationJobService::execute);
-        return ResponseEntity.ok("Order creation job scheduled");
+        return ResponseEntity.ok(orders);
     }
 }
