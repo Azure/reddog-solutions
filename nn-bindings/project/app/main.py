96f6ddb0bd8869d515fa1d85f1b33de2d97f40a8
Govind Kamtamneni <govind.k@microsoft.com>
2022-11-09 17:03:42 -0800

    fix gpt prompt composition for davinci model

--- nn-bindings/project/app/main.py
+++ nn-bindings/project/app/main.py
@@ -1,7 +1,18 @@
-from fastapi import Depends, FastAPI
-from .config import settings
-import openai
+import os
 
+import openai
+from fastapi import FastAPI, Request
+from pydantic import BaseSettings
+
+
+class Settings(BaseSettings):
+    openai.api_type = "azure"
+    openai.api_base = os.getenv("OPENAI_API_BASE")
+    openai.api_version = "2022-06-01-preview"
+    openai.api_key = os.getenv("OPENAI_API_KEY")
+
+
+settings = Settings()
 app = FastAPI()
 
 
@@ -10,33 +21,46 @@ async def pong():
     return {"status": "Ok"}
 
 
-@app.get("/generate/product-names", response_model=str)
-async def generate_product_names_route(prompt: str):
-    return generate_product_names(prompt)
-
-
-@app.get("/translate/to-french", response_model=str)
-async def generate_product_names(prompt: str):
-    response = openai.Completion.create(
-        engine="generate_description",
-        prompt=prompt,
-        temperature=0.5,
-        max_tokens=100,
-        top_p=1,
-        frequency_penalty=0,
-        presence_penalty=0,
-        stop=["\n"]
-    )
-    return response.choices[0].text
+@app.post("/generate/product-names")
+async def generate_product_names_route(req: Request):
+    # log prompt
+    req_info = await req.json()
+    print(req_info)
+    product_description = req_info["product_description"]
+    seed_words = req_info["seed_words"]
+    if not product_description or not seed_words:
+        print("No prompt provided")
+        return {"error": "No prompt provided"}
+    else:
+        composed_prompt = f'Product description:{product_description}\nSeed words:{seed_words}'
+        print(f'Prompt: {composed_prompt}')
+        gpt_output = Transformers.ask_davinci_2(composed_prompt)
+        return {"output": gpt_output}
 
 
 class Transformers():
-    def generate_product_names(self, prompt: str):
+    @staticmethod
+    def ask_davinci_2(prompt: str):
         response = openai.Completion.create(
             engine="text-davinci-002",
-            prompt=self,
+            prompt=prompt,
             temperature=0.8,
             max_tokens=60,
+            top_p=1,
+            frequency_penalty=0,
+            presence_penalty=0,
+            stop=None
+        )
+        print(f'GPT output: {response}')
+        return response.choices[0].text
+
+    @staticmethod
+    def ask_custom_model(prompt: str):
+        response = openai.Completion.create(
+            engine="generate_description",
+            prompt=prompt,
+            temperature=0,
+            max_tokens=64,
             top_p=1,
             frequency_penalty=0,
             presence_penalty=0,
