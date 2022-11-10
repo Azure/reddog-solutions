442a8e6d686b5d6ae48ef4fad5400904f411e0f1
Govind Kamtamneni <govind.k@microsoft.com>
2022-11-10 10:08:31 -0800

    add cors middleware for python endpoint

--- nn-bindings/project/app/main.py
+++ nn-bindings/project/app/main.py
@@ -2,6 +2,7 @@ import os
 
 import openai
 from fastapi import FastAPI, Request
+from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseSettings
 
 
@@ -14,6 +15,16 @@ class Settings(BaseSettings):
 
 settings = Settings()
 app = FastAPI()
+
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.get("/health")
