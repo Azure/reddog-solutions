e3c285e66869c78cc4809fc4816314b5134bcfc1
Govind Kamtamneni <govind.k@microsoft.com>
2022-11-14 08:35:19 -0800

    add local config server project

---
+++ ancillary/local-config-service/src/test/java/com/microsoft/gbb/reddog/localconfigservice/LocalConfigServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microsoft.gbb.reddog.localconfigservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LocalConfigServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
