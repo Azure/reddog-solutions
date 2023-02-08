e6629551ee67e4e335a3b3531ef1b0c5696b6c79
Joey Schluchter <jschluchter@gmail.com>
2023-02-08 09:02:48 -0500

    Knights of Columbus, that hurt!

--- scripts/start.sh
+++ scripts/start.sh
@@ -10,7 +10,13 @@ export DEPLOY_TARGET="$(echo $CONFIG | jq -r '.deploytarget')"
 
 # set initial variables
 export SUFFIX=$RANDOM
-export RG=reddog-java-spring-$SUFFIX
+export BASE=reddog-$USERNAME-$DEPLOY_TARGET
 export LOGFILE_NAME=".././outputs/${RG}.log"
+
+# cut down to a max of 20 characters
+export RGCUT=${BASE:0:22}
+
+# make it lowercase
+export RG=$(echo $RGCUT | tr '[:upper:]' '[:lower:]')
 
 ./walk-the-dog.sh $RG $LOCATION $SUFFIX $USERNAME $ADMIN_PASSWORD $DEPLOY_TARGET 2>&1 | tee -a $LOGFILE_NAME
