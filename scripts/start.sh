9f49cdd0aada0a658f767deb7c10c8b66f70fe44
Brian Redmond <brianisrunning@gmail.com>
2023-02-08 17:15:03 -0700

    cleanup some issues

--- scripts/start.sh
+++ scripts/start.sh
@@ -3,20 +3,16 @@ mkdir -p .././outputs
 # get params from config.json file
 export CONFIG="$(cat config.json | jq -r .)"
 
+# set variables
 export LOCATION="$(echo $CONFIG | jq -r '.location')"
 export USERNAME="$(echo $CONFIG | jq -r '.username')"
+export USERNAME_CUT=${USERNAME:0:5}
 export ADMIN_PASSWORD="$(echo $CONFIG | jq -r '.adminpassword')"
 export DEPLOY_TARGET="$(echo $CONFIG | jq -r '.deploytarget')"
-
-# set initial variables
 export SUFFIX=$RANDOM
-export BASE=reddog-$USERNAME-$DEPLOY_TARGET
-
-# cut down to a max of 20 characters
-export RGCUT=${BASE:0:20}
-
+export RG=reddog-$USERNAME_CUT-$DEPLOY_TARGET-$SUFFIX
 # make it lowercase
-export RG=$(echo $RGCUT | tr '[:upper:]' '[:lower:]')-$SUFFIX
+export RG=$(echo $RG | tr '[:upper:]' '[:lower:]')
 export LOGFILE_NAME=".././outputs/${RG}.log"
 
-./walk-the-dog.sh $RG $LOCATION $SUFFIX $USERNAME $ADMIN_PASSWORD $DEPLOY_TARGET 2>&1 | tee -a $LOGFILE_NAME
+./walk-the-dog.sh $RG $LOCATION $SUFFIX $USERNAME_CUT $ADMIN_PASSWORD $DEPLOY_TARGET 2>&1 | tee -a $LOGFILE_NAME
