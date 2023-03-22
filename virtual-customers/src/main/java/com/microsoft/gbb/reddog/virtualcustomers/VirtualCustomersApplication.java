bc16e8cb81a1f7d64ac2b333c219bcc8b56d5176
Govind Kamtamneni <govind.k@microsoft.com>
2023-03-22 08:28:37 -0700

    add feign client

--- virtual-customers/src/main/java/com/microsoft/gbb/reddog/virtualcustomers/VirtualCustomersApplication.java
+++ virtual-customers/src/main/java/com/microsoft/gbb/reddog/virtualcustomers/VirtualCustomersApplication.java
@@ -2,8 +2,12 @@ package com.microsoft.gbb.reddog.virtualcustomers;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableEurekaClient
+@EnableFeignClients
 public class VirtualCustomersApplication {
 
     public static void main(String[] args) {
